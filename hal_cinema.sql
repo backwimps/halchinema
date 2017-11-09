3fd0981936a5a3b20d5a08af967c7cc37b1ad755
ShuheiUsui <sworks.usui@gmail.com>
2017-11-09 12:52:41 +0900

    update sql

---
+++ hal_cinema.sql
@@ -0,0 +1,533 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: 2017 年 11 月 09 日 12:03
+-- サーバのバージョン： 5.6.35
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hal_cinema`
+--
+CREATE DATABASE IF NOT EXISTS `hal_cinema` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `hal_cinema`;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(6) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `authority` tinyint(1) NOT NULL,
+  `delete_flg` tinyint(1) NOT NULL,
+  `name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `campaigns`
+--
+
+CREATE TABLE `campaigns` (
+  `id` int(6) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `content` text NOT NULL,
+  `posting_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` tinyint(2) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `inquiries`
+--
+
+CREATE TABLE `inquiries` (
+  `id` int(6) NOT NULL,
+  `user_id` int(6) NOT NULL,
+  `inquiry_category_id` tinyint(2) NOT NULL,
+  `reply` int(6) NOT NULL,
+  `state_flg` tinyint(1) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `inquiry_categories`
+--
+
+CREATE TABLE `inquiry_categories` (
+  `id` tinyint(2) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `menus`
+--
+
+CREATE TABLE `menus` (
+  `id` tinyint(2) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `price` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `menu_reserves`
+--
+
+CREATE TABLE `menu_reserves` (
+  `id` tinyint(2) NOT NULL,
+  `menu_id` tinyint(2) NOT NULL,
+  `quantity` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(6) NOT NULL,
+  `option_id` tinyint(1) NOT NULL,
+  `sound` tinyint(1) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `screeningTime` int(3) NOT NULL,
+  `releaseDate` date NOT NULL,
+  `imagePath` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(6) NOT NULL,
+  `title` int(20) NOT NULL,
+  `content` text NOT NULL,
+  `posting_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `options`
+--
+
+CREATE TABLE `options` (
+  `id` tinyint(1) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `prices`
+--
+
+CREATE TABLE `prices` (
+  `id` int(6) NOT NULL,
+  `class_id` tinyint(1) NOT NULL,
+  `option_id` tinyint(1) NOT NULL,
+  `price` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `release_periods`
+--
+
+CREATE TABLE `release_periods` (
+  `movie_id` int(6) NOT NULL,
+  `theater_id` tinyint(2) NOT NULL,
+  `last_day` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `replies`
+--
+
+CREATE TABLE `replies` (
+  `id` int(6) NOT NULL,
+  `admin_id` int(6) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `reserves`
+--
+
+CREATE TABLE `reserves` (
+  `id` int(6) NOT NULL,
+  `user_id` int(6) NOT NULL,
+  `schedule_id` int(6) NOT NULL,
+  `class_id` tinyint(1) NOT NULL,
+  `menu_reserve` int(6) NOT NULL,
+  `seat_no` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int(6) NOT NULL,
+  `movie_id` int(6) NOT NULL,
+  `screen_id` int(6) NOT NULL,
+  `start_time` time(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `screens`
+--
+
+CREATE TABLE `screens` (
+  `id` int(6) NOT NULL,
+  `theater_id` tinyint(2) NOT NULL,
+  `audience` varchar(3) NOT NULL,
+  `screen_no` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `theater`
+--
+
+CREATE TABLE `theater` (
+  `id` tinyint(2) NOT NULL,
+  `name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(6) NOT NULL,
+  `nickname` varchar(10) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `mailAddress` varchar(100) NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `birthday` date NOT NULL,
+  `withdrawal` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `campaigns`
+--
+ALTER TABLE `campaigns`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `inquiry_category_id` (`inquiry_category_id`);
+
+--
+-- Indexes for table `inquiry_categories`
+--
+ALTER TABLE `inquiry_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu_reserves`
+--
+ALTER TABLE `menu_reserves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `menu_id` (`menu_id`);
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `option_id` (`option_id`),
+  ADD KEY `sound_id` (`sound`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `prices`
+--
+ALTER TABLE `prices`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `option_id` (`option_id`);
+
+--
+-- Indexes for table `release_periods`
+--
+ALTER TABLE `release_periods`
+  ADD KEY `movie_id` (`movie_id`),
+  ADD KEY `theater_id` (`theater_id`);
+
+--
+-- Indexes for table `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `reserves`
+--
+ALTER TABLE `reserves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `schedule_id` (`schedule_id`),
+  ADD KEY `class_id` (`class_id`);
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `movie_id` (`movie_id`),
+  ADD KEY `screen_id` (`screen_id`);
+
+--
+-- Indexes for table `screens`
+--
+ALTER TABLE `screens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `theater_id` (`theater_id`);
+
+--
+-- Indexes for table `theater`
+--
+ALTER TABLE `theater`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `campaigns`
+--
+ALTER TABLE `campaigns`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` tinyint(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `inquiry_categories`
+--
+ALTER TABLE `inquiry_categories`
+  MODIFY `id` tinyint(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` tinyint(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `menu_reserves`
+--
+ALTER TABLE `menu_reserves`
+  MODIFY `id` tinyint(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` tinyint(1) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `prices`
+--
+ALTER TABLE `prices`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `reserves`
+--
+ALTER TABLE `reserves`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `screens`
+--
+ALTER TABLE `screens`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `theater`
+--
+ALTER TABLE `theater`
+  MODIFY `id` tinyint(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `inquiries`
+--
+ALTER TABLE `inquiries`
+  ADD CONSTRAINT `inquiries_ibfk_1` FOREIGN KEY (`inquiry_category_id`) REFERENCES `inquiry_categories` (`id`),
+  ADD CONSTRAINT `inquiries_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- テーブルの制約 `menu_reserves`
+--
+ALTER TABLE `menu_reserves`
+  ADD CONSTRAINT `menu_reserves_ibfk_1` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`);
+
+--
+-- テーブルの制約 `movies`
+--
+ALTER TABLE `movies`
+  ADD CONSTRAINT `movies_ibfk_1` FOREIGN KEY (`option_id`) REFERENCES `options` (`id`);
+
+--
+-- テーブルの制約 `release_periods`
+--
+ALTER TABLE `release_periods`
+  ADD CONSTRAINT `release_periods_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`),
+  ADD CONSTRAINT `release_periods_ibfk_2` FOREIGN KEY (`theater_id`) REFERENCES `theater` (`id`);
+
+--
+-- テーブルの制約 `replies`
+--
+ALTER TABLE `replies`
+  ADD CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admins` (`id`);
+
+--
+-- テーブルの制約 `reserves`
+--
+ALTER TABLE `reserves`
+  ADD CONSTRAINT `reserves_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
+  ADD CONSTRAINT `reserves_ibfk_2` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`id`),
+  ADD CONSTRAINT `reserves_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- テーブルの制約 `schedules`
+--
+ALTER TABLE `schedules`
+  ADD CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`),
+  ADD CONSTRAINT `schedules_ibfk_2` FOREIGN KEY (`screen_id`) REFERENCES `screens` (`id`);
+
+--
+-- テーブルの制約 `screens`
+--
+ALTER TABLE `screens`
+  ADD CONSTRAINT `screens_ibfk_1` FOREIGN KEY (`theater_id`) REFERENCES `theater` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
