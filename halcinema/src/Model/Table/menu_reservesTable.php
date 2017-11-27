258f7eba9b48a893a83f16e41cd9d24fffbae706
ShuheiUsui <sworks.usui@gmail.com>
2017-11-27 14:47:03 +0900

    crear

--- halcinema/src/Model/Table/menu_reservesTable.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Model\Table;
-use Cake\ORM\Table;
-
-class menu_reservesTable extends Table {
-
-}
--- halcinema/src/Model/Table/relase_periodTable.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Model\Table;
-use Cake\ORM\Table;
-
-class relase_periodTable extends Table {
-
-}
