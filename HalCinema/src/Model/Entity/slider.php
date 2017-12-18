b22f22a4baca87779c7c9b5201b206854a5dab80
ShuheiUsui <sworks.usui@gmail.com>
2017-12-18 14:52:25 +0900

    edit entity

--- HalCinema/src/Model/Entity/slider.php
+++ HalCinema/src/Model/Entity/slider.php
@@ -5,27 +5,4 @@ use Cake\ORM\Entity;
 
 class Slider extends Entity{
 
-	public $id;
-	public $movie_id;
-	public $movie_title;
-	public $movie_imagePath;
-
-	public function _setSlider($row) {
-		$this->id = $row['id'];
-		$this->movie_id = $row['movie_id'];
-		$this->movie_title = $row['movie']['title'];
-		$this->movie_imagePath = $row['movie']['imagePath'];
-	}
-
-	public function _getSlider() {
-		$sliders = array(
-			'id' => $this->id,
-			'movie_id' => $this->movie_id,
-			'movie_title' => $this->movie_title,
-			'movie_imagePath' => $this->movie_imagePath
-		);
-
-		return $sliders;
-	}
-
 }
