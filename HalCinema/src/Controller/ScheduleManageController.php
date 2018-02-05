177a8718c74554081fd7d86deffcf1be28df3e4f
ShuheiUsui <sworks.usui@gmail.com>
2018-02-05 15:20:58 +0900

    コントローラのメソッド追加

--- HalCinema/src/Controller/ScheduleManageController.php
+++ HalCinema/src/Controller/ScheduleManageController.php
@@ -1,5 +1,8 @@
 <?php
 namespace App\Controller;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+use App\Utils\Session;
 
 class ScheduleManageController extends AppController{
 	public function initialize(){
@@ -13,4 +16,18 @@ class ScheduleManageController extends AppController{
 		// $dao = new DataAccessObject;
 
 	}
+
+	public function add(){
+		$this->autoRender = false;
+
+		$scheduleTable = TableRegistry::get('Schedules');
+
+		$schedule = array();
+
+		$this->loadModel('Schedules');
+		$entity = $this->Schedules->newEntity($schedule);
+
+		$scheduleTable->save($entity);
+	}
+
 }
